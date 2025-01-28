4bb53124f21566560941648a8857ce283d4dddf6
cxctxs_jxck <pedroelchico0@gmail.com>
2025-01-28 19:55:16 +0100

    Add Black Flash and Domain Expansion commands with random GIFs

--- Sleepy.py
+++ Sleepy.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import random
 
 # Cargar variables del archivo .env
 load_dotenv()
@@ -17,27 +18,60 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
 
-# Comando básico: !hola
-@bot.command()
-async def hola(ctx):
-    await ctx.send(f'¡Hola, {ctx.author.mention}!')
+# Comando bf!
+@bot.command(name="bf")
+async def black_flash(ctx, user: discord.Member):
+    # Lista de GIFs predefinidos
+    gifs = [
+        "https://media1.tenor.com/m/-nZnQBzGa7EAAAAd/jujutsu-kaisen-jujutsu-kaisen-season-2.gif",
+        "https://media1.tenor.com/m/IfLc43H57IMAAAAd/jjk-jjk-s2.gif",
+        "https://media1.tenor.com/m/SA78kvgb6SIAAAAd/nanami-nanami-kento.gif", 
+        "https://media1.tenor.com/m/K4zh-8HS-GYAAAAd/satoru-gojo-gojo-satoru.gif",
+        "https://media1.tenor.com/m/wgHrYBAAuzUAAAAd/jjk0-yuta.gif"
+    ]
+    
+    # Seleccionar un GIF aleatoriamente
+    gif_seleccionado = random.choice(gifs)
 
-# Comando básico: !adios
-@bot.command()
-async def adios(ctx):
-    await ctx.send('¡Adiós! ¡Que tengas un buen día!')
+    # Crear un embed
+    embed = discord.Embed(
+        title="¡Black Flash!",
+        description=f"{ctx.author.mention} usó **Black Flash** contra {user.mention} ⚡",
+        color=discord.Color.red()
+    )
+    embed.set_image(url=gif_seleccionado)
 
-# Comando básico: !hora
-@bot.command()
-async def hora(ctx):
-    from datetime import datetime
-    ahora = datetime.now()
-    await ctx.send(f'Son las {ahora.strftime("%H:%M")}.')
+    # Enviar el embed
+    await ctx.send(embed=embed)
+    
+# Comando de
+@bot.command(name="de")
+async def black_flash(ctx, user: discord.Member):
+    # Lista de GIFs predefinidos
+    gifs = [
+        "https://media1.tenor.com/m/MuMLDWrW95gAAAAd/gojo-domain-expansion.gif",
+        "https://media1.tenor.com/m/RAp5YpmEH5EAAAAd/jujutsu-kaisen-shibuya-arc-sukuna-shibuya-arc.gif",
+        "https://media1.tenor.com/m/G_HN1fYl61kAAAAd/domain-expansion-yuta.gif", 
+        "https://media1.tenor.com/m/09i4-rohmdAAAAAd/kenjaku-geto-suguru.gif",
+        "https://media1.tenor.com/m/3fzEJTA3ykUAAAAd/hakari-kinji-kinji-hakari.gif",
+        "https://media1.tenor.com/m/EJW3gcpVvWgAAAAd/jogo-domain-expansion.gif",
+        "https://media1.tenor.com/m/rzLycKqpA_EAAAAd/mahito-domain-expansion.gif",
+    ]
+    
+    # Seleccionar un GIF aleatoriamente
+    gif_seleccionado = random.choice(gifs)
 
-# Comando básico: !repetir
-@bot.command()
-async def repetir(ctx, *, mensaje: str):
-    await ctx.send(f'Has dicho: {mensaje}')
+    # Crear un embed
+    embed = discord.Embed(
+        title="¡EXPANSION DE DOMINIO!",
+        description=f"{ctx.author.mention} usó su **Expansion de dominio** contra {user.mention} 😯😯",
+        color=discord.Color.blue()
+    )
+    embed.set_image(url=gif_seleccionado)
+
+    # Enviar el embed
+    await ctx.send(embed=embed)
+    
 
 # Ejecuta el bot con el token cargado
 bot.run(TOKEN)
